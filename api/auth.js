ba1d8c2eb4773cb76123a6659ced4b5eb20afcd1
steinskeeper <dpelpsykongroo@gmail.com>
2021-11-20 16:43:24 +0530

    seller signup + image upload

--- api/auth.js
+++ api/auth.js
@@ -3,6 +3,34 @@ var router = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const bcrypt = require("bcryptjs");
+var multer = require("multer")
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+
+        if (file.mimetype === "image/png" || file.mimetype === "image/jpeg" || file.mimetype === "image/jpg") {
+            cb(null, "./static/shop");
+        }
+
+        else {
+            console.log(file.mimetype);
+            cb({ error: "Mime type not supported" });
+        }
+    },
+    filename: function (req, file, cb) {
+        let extArray = file.mimetype.split("/");
+        let extension = extArray[extArray.length - 1];
+        var date_now = new Date();
+        let dd = String(date_now.getDate()).padStart(2, "0")
+        let mm = String(date_now.getMonth() + 1).padStart(2, "0")
+        let yy = date_now.getFullYear()
+        let timestamp = dd + "-" + mm + "-" + yy + "";
+        cb(
+            null,
+            file.originalname + "-" + file.fieldname + "-" + timestamp + "." + extension
+        );
+    }
+});
+var upload = multer({ storage: storage });
 
 router.get('/hello', (req, res) => {
     res.send('Auth!')
@@ -57,15 +85,23 @@ router.post("/addbuyer", async function (req, res, next) {
         });
     }
 });
-router.post("/addseller", async function (req, res, next) {
+router.post("/addseller", [upload.any()], async function (req, res, next) {
     try {
-        const { username, password } = req.body;
-        const encryptedPassword = await bcrypt.hash(password, 10);
+        const file = req.files;
+        const itemimg = file.slice(-1);
+        const cred = JSON.parse(req.body.cred);
+        const encryptedPassword = await bcrypt.hash(cred.password, 10);
 
         const newuser = await prisma.users.create({
             data: {
-                username: username,
-                role: "buyer",
+                username: cred.username,
+                role: "seller",
+                desc:cred.desc,
+                name:cred.name,
+                category:cred.category,
+                location: cred.location,
+                banner: itemimg[0].path,
+                picture: itemimg[0].path,
                 password: encryptedPassword,
             },
         });
